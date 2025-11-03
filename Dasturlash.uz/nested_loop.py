4e9f269b6e49aae68a6eb534737d8e2159039e4d
samandar-tursunov <tursonovsamandar455@gmail.com>
2025-11-03 16:09:21 +0500

    6 answers

---
+++ Dasturlash.uz/nested_loop.py
@@ -0,0 +1,38 @@
+#1 
+# a = int(input("number = "))
+# s = 0
+# for i in range(1,a+1):
+#     s = s + (1/i)
+# print(s)
+
+#2
+# a = int(input("n = "))
+# for i in range(a):
+#     print(a*"*")
+    
+#3
+# n = int(input("n = "))
+# for i in range(n+1):
+#     for j in range(n+1):
+#         print(f"({i},{j})", end = " ")
+#     print("\n")
+
+#4 Sonlar Juftliga C ni chiqarish
+# n  =  int(input("n = "))
+# for i in range(n+1):
+#     for j in range(n+1):
+#         if i == 0 or j == 0 or i == 9:
+#             print(f"({i},{j})", end=" ")
+#     print()
+
+#5 Sonlar juftligida ustunlar
+# n=int(input("n - "))
+# for i in range(n+1):
+#     for j in range(n+1):
+#         if i == 0 or i ==9 or j%2 == 0:
+#             print(f"({i},{j})", end=" ")
+#         else:
+#             print(end="      ")
+#     print()
+
+#6
