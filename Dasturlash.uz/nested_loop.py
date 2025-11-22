aae900d5daca0a04c40126d8b88d48f7213d1da4
Samandar Tursunov <137744845+samandar-tursunov@users.noreply.github.com>
2025-11-23 01:28:28 +0500

    Enable execution of nested loop examples
    Updated multiple sections of nested_loop.py to remove comment markers and enable code execution.

--- Dasturlash.uz/nested_loop.py
+++ Dasturlash.uz/nested_loop.py
@@ -1,98 +1,98 @@
 #1 
-# a = int(input("number = "))
-# s = 0
-# for i in range(1,a+1):
-#     s = s + (1/i)
-# print(s)
+a = int(input("number = "))
+s = 0
+for i in range(1,a+1):
+    s = s + (1/i)
+print(s)
 
 #2
-# a = int(input("n = "))
-# for i in range(a):
-#     print(a*"*")
+a = int(input("n = "))
+for i in range(a):
+    print(a*"*")
     
 #3
-# n = int(input("n = "))
-# for i in range(n+1):
-#     for j in range(n+1):
-#         print(f"({i},{j})", end = " ")
-#     print("\n")
+n = int(input("n = "))
+for i in range(n+1):
+    for j in range(n+1):
+        print(f"({i},{j})", end = " ")
+    print("\n")
 
 #4 
-# n  =  int(input("n = "))
-# for i in range(n+1):
-#     for j in range(n+1):
-#         if i == 0 or j == 0 or i == 9:
-#             print(f"({i},{j})", end=" ")
-#     print()
+n  =  int(input("n = "))
+for i in range(n+1):
+    for j in range(n+1):
+        if i == 0 or j == 0 or i == 9:
+            print(f"({i},{j})", end=" ")
+    print()
 
 #5 
-# n=int(input("n - "))
-# for i in range(n+1):
-#     for j in range(n+1):
-#         if i == 0 or i ==9 or j%2 == 0:
-#             print(f"({i},{j})", end=" ")
-#         else:
-#             print(end="      ")
-#     print()
+n=int(input("n - "))
+for i in range(n+1):
+    for j in range(n+1):
+        if i == 0 or i ==9 or j%2 == 0:
+            print(f"({i},{j})", end=" ")
+        else:
+            print(end="      ")
+    print()
 
 #6 List of numbers
-# n = int(input("n = "))
-# s = 0
-# l = []
-# for i in range(1,(n*n + 1)):
-#     print(f"{s}\t", end=" ")
-#     s +=1
-#     l.append(s)
-#     if len(l)%10 == 0:
-#         print()
+n = int(input("n = "))
+s = 0
+l = []
+for i in range(1,(n*n + 1)):
+    print(f"{s}\t", end=" ")
+    s +=1
+    l.append(s)
+    if len(l)%10 == 0:
+        print()
 
 #7 Dioganal
-# n = int(input("n = "))
-# for i in range(n):
-#     print("*")
-#     for j in range(n):
-#         print(" "*i, end = " ")
+n = int(input("n = "))
+for i in range(n):
+    print("*")
+    for j in range(n):
+        print(" "*i, end = " ")
 
 #8 numbers dioganali 
-# n = int(input("n = "))
-# for i in range(n+1):
-#     print(f"({i},{i})")
-#     for j in range(n+1):
-#         print(i*" ", end = " ")
+n = int(input("n = "))
+for i in range(n+1):
+    print(f"({i},{i})")
+    for j in range(n+1):
+        print(i*" ", end = " ")
         
 #9 inverted dioganal
-# n = int(input("n = "))
-# for i in range(n):
-#     print(n*" ","*")
-#     n -=1
+n = int(input("n = "))
+for i in range(n):
+    print(n*" ","*")
+    n -=1
   
 #10 Right-angled triangle
-# n = int(input("n - "))
-# for i in range(n+1):
-#     print("*"*i)
+n = int(input("n - "))
+for i in range(n+1):
+    print("*"*i)
 
 #11 Inverted right-angled triangle
-# n = int(input("n = "))
-# for i in range(n+1):
-#     print(n*"*")
-#     n -=1
+n = int(input("n = "))
+for i in range(n+1):
+    print(n*"*")
+    n -=1
 
 #12 Number triangle
-# n = int(input("n = "))
-# for i in range(1, (n+1)):
-#     for j in range(1, i+1):
-#         print(j, end = "")
-#     print()
+n = int(input("n = "))
+for i in range(1, (n+1)):
+    for j in range(1, i+1):
+        print(j, end = "")
+    print()
 
 #13 triangle of numbers options
-# n = int(input("n = "))
-# for i in range(n+1):
-#     print(str(i)*i)
+n = int(input("n = "))
+for i in range(n+1):
+    print(str(i)*i)
 
 #14 Paralellogramm
-# n = int(input("n = "))
-# for i in range(n+1):
-#     print(i*" ", n*"*")
+n = int(input("n = "))
+for i in range(n+1):
+    print(i*" ", n*"*")
     
 #15 Rectangular shape
 #*****
@@ -100,53 +100,53 @@
 #*   *
 #*   *
 #*****
-# n = int(input("n = "))
-# for i in range(n):
-#     print("\n")
-#     if i==0 or i==n-1:
-#         print(n*"*", end="")
-#     else:
-#         print("*"+(n-2)*" ", end="*")
+n = int(input("n = "))
+for i in range(n):
+    print("\n")
+    if i==0 or i==n-1:
+        print(n*"*", end="")
+    else:
+        print("*"+(n-2)*" ", end="*")
 
 #16
-# n = 5
-# for i in range(1,n+1):
-#     print("-"*i + "*" + "+"*(n-1))
-#     n-=1
+n = 5
+for i in range(1,n+1):
+    print("-"*i + "*" + "+"*(n-1))
+    n-=1
 
 #17 * star
-# n = 5
-# for i in range(n):
-#     for j in range(n):
-#        if i ==j or j == n - i -1:
-#            print("*", end = " ")
-#        else:
-#            print(" ", end = " ")
-#     print()
+n = 5
+for i in range(n):
+    for j in range(n):
+       if i ==j or j == n - i -1:
+           print("*", end = " ")
+       else:
+           print(" ", end = " ")
+    print()
 
 # 2nd option
-# for i in range(n):
-#     line = [" "] * n
-#     line[i] = "*"
-#     line[n - i - 1] = "*"
-#     print("".join(line))
+for i in range(n):
+    line = [" "] * n
+    line[i] = "*"
+    line[n - i - 1] = "*"
+    print("".join(line))
 
 #18 inverted parallelogram
-# n = 5
-# for i in range(n,0,-1):
-#     print(" "*(i-1) + "*"*5)
+n = 5
+for i in range(n,0,-1):
+    print(" "*(i-1) + "*"*5)
 
     
 #19 Unusual parallelogram
-# n = 5
-# for i in range(n,0, -1):
-#     if i == n or i == 1:
-#         print(i*" " + "*"*n)
-#     else:
-#         print(i*" " + "*" + (n-2)*" " + "*")
+n = 5
+for i in range(n,0, -1):
+    if i == n or i == 1:
+        print(i*" " + "*"*n)
+    else:
+        print(i*" " + "*" + (n-2)*" " + "*")
 
 #20 Triangle
-# n = 5
-# for i in range(n+1):
-#     print(" "*(n-i) + "* "*i)
+n = 5
+for i in range(n+1):
+    print(" "*(n-i) + "* "*i)
    
